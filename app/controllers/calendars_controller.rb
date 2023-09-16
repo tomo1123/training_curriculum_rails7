85885bce5bef70e9f2a292929216c324ceed3328
tomo1123 <tomocruise01@gmail.com>
2023-09-16 19:55:30 +0900

    Issu4~6 の確認

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -39,6 +39,7 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
+      
       wday_num = (@todays_date + x).wday
       if wday_num >= 7
         wday_num = wday_num -7
